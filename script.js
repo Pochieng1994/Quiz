d704d7696f92708c19dc7169557cf7044fab3e9e
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-05-17 20:51:12 -0400

    Finished working on quiz for today pushing up to github

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Create an HTML form with a three-question quiz
+-Each question should have four multiple-choice answers to choose from.
+-When the user submits the quiz, add a message underneath each question 
+letting them know if they got it right/wrong (give them the correct answer).
+*/
+
+let capital = document.querySelector('#capital');
+let language = document.querySelector('#language');
+let area = document.querySelector('#area');
+let submit = document.querySelector('#submit');
+
+
