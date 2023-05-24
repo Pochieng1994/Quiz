c2c3c8f06f6f84c112c11a81e2b9877a951d7e60
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-05-23 23:18:19 -0400

    Finished working on quiz project for the day pushing up to Github

--- script.js
+++ script.js
@@ -9,4 +9,50 @@ let language = document.querySelector('#language');
 let area = document.querySelector('#area');
 let submit = document.querySelector('#submit');
 
+let correctAnswer1 = document.getElementById('bangkok');
+let correctAnswer2 = document.getElementById('thai');
+let correctAnswer3 = document.getElementById('southern-thailand');
+
+let myForm = document.querySelector('#form');
+
+let numberOfAttempts = 0;
+
+submit.addEventListener('click', function() {
+
+
+  if(correctAnswer1.checked === true) { 
+    capital.textContent = 'Congrats you answered correctly, the capital Bangkok!';
+    capital.style.color = 'green';
+  } else {
+    capital.textContent = 'Your answer is not correct, the correct is Bangkok';
+    capital.style.color = 'red';
+  }
+
+  if(correctAnswer2.checked === true) {
+    language.textContent = 'Congrats you answered correctly, the official language is Thai!';
+    language.style.color = 'green';
+  } else {
+    language.textContent = 'Your answer is not correct, the official language is Thai!';
+    language.style.color = 'red';
+  }
+
+  if(correctAnswer3.checked === true) {
+    area.textContent = 'Congrats you answered correctly, Phuket is in fact located in Southern Thailand!';
+    area.style.color = 'green';
+  } else {
+    area.textContent = 'Your answer is not correct, Phuket is located in Southern Thailand';
+    area.style.color = 'red';
+  }
+
+})
+
+myForm.addEventListener('submit', function(event) {
+  event.preventDefault;
+})
+
+
+
+
+
+
 
